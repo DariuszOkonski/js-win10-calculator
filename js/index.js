730097c4a4cb1efa7d64ce9f58e7f7184cb588b8
Dariusz Okonski <darek200180@gmail.com>
2020-05-06 14:58:22 +0100

    add some functions

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
 class Calculator {
     constructor(){
+        this.currentNumber = "0";
+        
         this.dataMobileHistory = null;
         this.dataMemoryButtons = null;
-
+        this.dataMemorySingleButton = null;
+        this.dataNumbericBtns = null;
+        this.dataDisplayCurrentNumber = null;
+        this.dataClearCurrentNumber = null;
 
         this.uiElements = {
             dataBtnHistoryMobile: '[data-btn-history-mobile]',
             dataMobileHistory: '[data-mobile-history]',
-            dataMemoryButtons:  '[data-memory-buttons]'
+            dataMemoryButtons:  '[data-memory-buttons]',
+            dataMemorySingleButton: '[data-memory-single-button]',
+            dataNumbericBtns: '[data-numeric-btns]',
+            dataDisplayCurrentNumber: '[data-display-current-number]',
+            dataClearCurrentNumber: '[data-clear-current-number]',
         }
     
     }
@@ -15,16 +24,57 @@ class Calculator {
     initialization() {
         this.dataMobileHistory = document.querySelector(this.uiElements.dataMobileHistory);
         this.dataMemoryButtons = document.querySelector(this.uiElements.dataMemoryButtons);
-
+        this.dataMemorySingleButton = document.querySelectorAll(this.uiElements.dataMemorySingleButton);
+        this.dataNumbericBtns = document.querySelectorAll(this.uiElements.dataNumbericBtns);
+        this.dataDisplayCurrentNumber = document.querySelector(this.uiElements.dataDisplayCurrentNumber);
+        this.dataClearCurrentNumber = document.querySelector(this.uiElements.dataClearCurrentNumber);
         this.addEventListeners();
     }
 
     addEventListeners() {
         document.querySelector(this.uiElements.dataBtnHistoryMobile).addEventListener('click', this.toggleDataMobileHistory);
+        this.dataClearCurrentNumber.addEventListener('click', this.clearCurrentNumber);
+        
+        this.dataMemorySingleButton.forEach(singleButton => singleButton.addEventListener('click', this.dataMemorySingleButtonClick));
+        this.dataNumbericBtns.forEach(singleButton => singleButton.addEventListener('click', this.dataNumbericBtnsClick));
+
+    }
+
+    clearCurrentNumber = () => {
+        this.currentNumber = "0";
+        this.displayCurrentNumber(this.currentNumber);
+    }
+
+    displayCurrentNumber = (number) => {
+        this.dataDisplayCurrentNumber.innerText = this.numberWithCommas(number);
+    }
+
+    dataNumbericBtnsClick = (e) => {
+        
+        if(this.currentNumber === "0") {
+            this.currentNumber = e.target.dataset.numericBtns;
+            this.displayCurrentNumber(this.currentNumber);
+            return;
+        }
+        
+        if(this.currentNumber.length >= 10)
+            return;
+
+        this.currentNumber += e.target.dataset.numericBtns;
+        this.displayCurrentNumber(this.currentNumber);
+    }
+
+    dataMemorySingleButtonClick = (e) => {
+        console.log(e.target)
+        console.log('data memory single button click');
     }
 
     toggleDataMobileHistory = (e) => {
         this.dataMobileHistory.classList.toggle('active');
         this.dataMemoryButtons.classList.toggle('active');
+    }
+
+    numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 }
