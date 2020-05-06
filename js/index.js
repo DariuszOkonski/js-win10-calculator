48aa513a7502160e37ce2836d7af31d1cb5f9e36
Dariusz Okonski <darek200180@gmail.com>
2020-05-06 13:17:04 +0100

    add toggleDataMobileHistory

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+class Calculator {
+    constructor(){
+        this.dataMobileHistory = null;
+
+        this.uiElements = {
+            dataBtnHistoryMobile: '[data-btn-history-mobile]',
+            dataMobileHistory: '[data-mobile-history]',
+        }
+    
+    }
+
+    initialization() {
+        this.dataMobileHistory = document.querySelector(this.uiElements.dataMobileHistory);
+
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        document.querySelector(this.uiElements.dataBtnHistoryMobile).addEventListener('click', this.toggleDataMobileHistory);
+    }
+
+    toggleDataMobileHistory = (e) => {
+        this.dataMobileHistory.classList.toggle('active');
+    }
+}
