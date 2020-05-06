b648b409ced394f0cb1c12ab1016bbb5a18aa3ed
Dariusz Okonski <darek200180@gmail.com>
2020-05-06 13:24:25 +0100

    add reference to dataMemoryButtons

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 class Calculator {
     constructor(){
         this.dataMobileHistory = null;
+        this.dataMemoryButtons = null;
+
 
         this.uiElements = {
             dataBtnHistoryMobile: '[data-btn-history-mobile]',
             dataMobileHistory: '[data-mobile-history]',
+            dataMemoryButtons:  '[data-memory-buttons]'
         }
     
     }
 
     initialization() {
         this.dataMobileHistory = document.querySelector(this.uiElements.dataMobileHistory);
+        this.dataMemoryButtons = document.querySelector(this.uiElements.dataMemoryButtons);
 
         this.addEventListeners();
     }
@@ -21,5 +25,6 @@ class Calculator {
 
     toggleDataMobileHistory = (e) => {
         this.dataMobileHistory.classList.toggle('active');
+        this.dataMemoryButtons.classList.toggle('active');
     }
 }
